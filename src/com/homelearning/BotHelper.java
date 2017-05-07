2998878b3bf8dfc8dd53d00a8b08ac1f85ec65cc
Vitaliy Ostapenko <soul33d@gmail.com>
2017-05-07 16:31:04 +0300

    Changed sleep time before pasting table

--- src/com/homelearning/BotHelper.java
+++ src/com/homelearning/BotHelper.java
@@ -33,7 +33,7 @@ public class BotHelper {
         multipleLeftMouseClick(table.getSize()/2 - 1);
 
         try {
-            Thread.sleep(5000);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
